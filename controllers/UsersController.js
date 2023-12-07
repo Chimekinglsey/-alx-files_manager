d8e5227b67a870552709cc93acd638320f8f9200
chimekinglsey <onlinekingsley@gmail.com>
2023-12-07 01:08:36 +0100

    rewritten userscontroller

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -4,25 +4,62 @@ const dbClient = require('../utils/db');
 const usersController = {
   postNew: async (req, resp) => {
     const { email, password } = req.body;
+
     if (!email) {
-      resp.status(400).json({ error: 'Missing email' });
+      return resp.status(400).json({ error: 'Missing email' });
     }
+
     if (!password) {
-      resp.status(400).json({ error: 'Missing password' });
+      return resp.status(400).json({ error: 'Missing password' });
     }
+
     try {
       const query = { email };
       const emailPresent = await dbClient.connection.collection('user').findOne(query);
+
       if (emailPresent) {
-        resp.status(400).json({ error: 'Already exist' });
+        return resp.status(400).json({ error: 'Already exists' });
       }
+
       const hashedPassword = sha1(password);
-      const newUser = await dbClient.connection.collection('user').insertOne({ email: email, password: hashedPassword });
-      resp.status(201).json({ id: newUser.insertedId, email: email });
+      const newUser = await dbClient.connection.collection('user').insertOne({ email, password: hashedPassword });
+
+      return resp.status(201).json({ id: newUser.insertedId, email });
     } catch (error) {
-      resp.status(500).json({ error: 'Error while trying to add users' });
+      console.error(`Error while creating user: ${error}`);
+      return resp.status(500).json({ error: 'Error while creating user' });
     }
   },
 };
 
 module.exports = usersController;
+
+// const sha1 = require('sha1');
+// const dbClient = require('../utils/db');
+
+// const usersController = {
+//   postNew: async (req, resp) => {
+//     const { email, password } = req.body;
+//     if (!email) {
+//       resp.status(400).json({ error: 'Missing email' });
+//     }
+//     if (!password) {
+//       resp.status(400).json({ error: 'Missing password' });
+//     }
+//     try {
+//       const query = email;
+//       const emailPresent = await dbClient.connection.collection('user').findOne(query);
+//       if (emailPresent) {
+//         resp.status(400).json({ error: 'Already exist' });
+//       }
+//       const hashedPassword = sha1(password);
+//       const newUser = await dbClient.connection.collection('user').insertOne(
+// { email, password: hashedPassword });
+//       resp.status(201).json({ id: newUser.insertedId, email });
+//     } catch (error) {
+//       resp.status(500).json({ error: 'Error while trying to add users' });
+//     }
+//   },
+// };
+
+// module.exports = usersController;
